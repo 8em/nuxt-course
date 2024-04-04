f3f9adbc7bf933e7c7efc03b3acc58af1d44a7a9
8em <jpeg.oleg@gmail.com>
2024-04-05 00:13:08 +0300

    projects

--- components/project-list.vue
+++ components/project-list.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
-        SOme
-    </div>
+  <div>Project</div>
+
+  <section v-if="pending">Loading...</section>
+  <section v-else-if="error">Something went wrong!</section>
+  <section v-else>
+    <ul>
+      <li v-for="repo in data" :key="repo.id">
+        <div>{{ repo.name }}</div>
+      </li>
+    </ul>
+  </section>
 </template>
+<script setup>
+const { data, error, pending } = await useFetch(
+  'https://api.github.com/users/piotr-jura-udemy/repos'
+)
+
+console.log(data)
+</script>
